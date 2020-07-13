a5544d1069629dc065c53581335650442c7ac884
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-07-13 09:37:07 -0300

    Operador ternario

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //logico ? valor se verdadeiro : valor se falso
+
+            int x=5,  y = 10;
+
+            Console.WriteLine(x < y ? "Sim" : "Não");
+        }
+    }
+}
