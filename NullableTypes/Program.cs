123eaaa171c3f5eb05f66445496467d83c99aaae
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-07-13 09:30:44 -0300

    Nullable Types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;//atribui valor null a variavel
+
+            if (x != null)
+            {
+                //faz algo
+            }
+            else
+            {
+                //x possui valor nulo
+            }
+
+        }
+    }
+}
